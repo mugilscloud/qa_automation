83c7ffbe311ddbf385d8f63f9dec43f2df605861
mugilscloud <mugilscloud@gmail.com>
2016-09-12 08:26:15 -0700

    created new api test and modified ecommerce test

--- Ecommerce-Order-Test/src/test/java/com/apex/ecommerce/test/GoogleLoginTestNG.java
+++ Ecommerce-Order-Test/src/test/java/com/apex/ecommerce/test/GoogleLoginTestNG.java
@@ -34,7 +34,7 @@ public class GoogleLoginTestNG {
 		WebElement nextBtn2 = driver.findElement(By.id("signIn"));
 		nextBtn2.click();
 		Thread.currentThread().sleep(10000);
-		System.out.println("hi trying merge");
+		System.out.println("hi");
 		
 		String title = driver.getTitle();
 		Assert.assertEquals(title.contains("Inbox"), true);
